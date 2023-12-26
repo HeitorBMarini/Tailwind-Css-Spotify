d88fb424de7b063d0d106832c4fad879002b04e6
HeitorBMarini <mariniheitor@gmail.com>
2023-12-26 17:27:34 -0300

    Estlizando página com Icones da Lucide, inserindo imagens e organizando playlist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,92 @@
-
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
-          sidebar
+        <aside className="w-72 bg-zinc-950 p-6">
+
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full'/>
+            <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+            <div className='w-3 h-3 bg-green-500 rounded-full'/>
+          </div>
+
+          <nav className="space-y-5 mt-10">
+            <a href="/" className="flex items-center gap-2 text-sm font-semibold text-zinc-200 ">
+              <HomeIcon/>
+              Home
+            </a>
+          
+
+          
+            <a href="" className="flex items-center gap-2 text-sm font-semibold  text-zinc-200 ">
+              <Search/>
+              Search
+            </a>
+         
+
+         
+            <a href="" className="flex items-center gap-2 text-sm font-semibold  text-zinc-200 ">
+              <Library/>
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2'>
+              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+              <a href="" className='text-sm text-zinc-400  hover:text-zinc-100'>Músicas curtidas</a>
+
+          </nav>
+
         </aside>
         <main className="flex-1 p-6">
-          main
+            <div className='flex items-center gap-4'>
+              <button className=' rounded-full bg-black/20 p-1'>
+                <ChevronLeft/>
+              </button>
+
+              <button  className=' rounded-full bg-black/20 p-1'>
+                <ChevronRight/>
+              </button>
+
+            </div>
+
+            <h1 className='font-semibold text-3xl mt-10'> Good Afternoon</h1>
+
+            <div className='grid grid-cols-3 gap-6 mt-5'>
+              <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+              <a  href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+              <a   href='#'className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+              <a   href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+              <a  href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+              <a  href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'> 
+                <Image src="/album-skillet.png" width={104} height={104} alt="Capa album Skillet"/>
+                <strong>Skillet</strong>
+              </a>
+            </div>
+          
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
